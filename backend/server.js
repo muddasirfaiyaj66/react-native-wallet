5ab33c49938dfa8a69447fe652c5750d51eef0e4
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2025-05-30 18:08:39 +0600

    backend initial setup done . database use postgresql

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express()
+const PORT = process.env.PORT || 5001;
+
+
+
+app.get('/',(req,res)=>{
+    res.send("It Working")
+})
+app.listen(PORT,()=>{
+    console.log(`server is running on PORT:${PORT}`);
+    
+})
