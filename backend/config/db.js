a10a829962fc1d4643cdeb60a51a8488f5cf2c9f
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2025-05-30 18:43:06 +0600

    database connected successfully

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,4 +2,4 @@ import {neon} from "@neondatabase/serverless";
 import 'dotenv/config';
 
 //create a SQL Connection using our DB URL
-export const sql = neon(process.env.DATABASE_URL)
+export  const sql = neon(process.env.DATABASE_URL)
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,41 @@
 import express from 'express';
 import dotenv from "dotenv";
+import {sql} from './config/db.js'
 
 dotenv.config();
 const app = express()
 const PORT = process.env.PORT || 5001;
 
+async function initDB() {
+    try {
+        await sql`CREATE TABLE IF NOT EXISTS transactions(
+        id SERIAL PRIMARY KEY,
+        user_id VARCHAR(255) NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        amount DECIMAL(10,2) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        
+        )`
+        console.log('DATABASE INITIALIZED SUCCESSFULLY');
+        
 
+    } catch (error) {
+        console.log(`ERROR INITIALIZING DB ${error}`);
+        process.exit(1)
+        
 
-app.get('/',(req,res)=>{
+    }
+
+}
+
+app.get('/', (req, res) => {
     res.send("It Working")
 })
-app.listen(PORT,()=>{
+
+initDB().then(()=>{
+  app.listen(PORT, () => {
     console.log(`server is running on PORT:${PORT}`);
-    
+
+})  
 })
